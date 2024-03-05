0588ea68bd2a4888c8dc0dc6129d9c6694f9505f
Harshal <59302243+HARSHALK2598@users.noreply.github.com>
2024-03-04 20:12:51 -0500

    Create Container-With-Most-Water.cpp

---
+++ Week1/Container-With-Most-Water.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int start = 0, end = height.size() - 1, maxArea = -1, currArea = -1;
+        while(start < end){
+            currArea = min(height[start], height[end]) * (end - start);
+            maxArea = currArea > maxArea ? currArea : maxArea;
+            if (height[start] < height[end]) start += 1;
+            else end -= 1;
+        }
+        return maxArea;
+    }
+};
